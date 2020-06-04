23d865ba1c7e074a423940aeab32cc1a8b290b01
Yuho Jeong <yuho8437@unist.ac.kr>
2020-06-04 15:29:10 +0900

    Updates

---
+++ precache-manifest.62a5e13645eca513b1f7f0d489c145f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a438443d503cd3e3eebeabac92cda850",
+    "url": "/covid-correlation/index.html"
+  },
+  {
+    "revision": "28f9050e47190defaa6d",
+    "url": "/covid-correlation/static/css/2.4c6ee7a4.chunk.css"
+  },
+  {
+    "revision": "9572c3d4713c655b6541",
+    "url": "/covid-correlation/static/css/main.deea4af0.chunk.css"
+  },
+  {
+    "revision": "28f9050e47190defaa6d",
+    "url": "/covid-correlation/static/js/2.df818563.chunk.js"
+  },
+  {
+    "revision": "72f0228dba0f081339e2769a48478d0f",
+    "url": "/covid-correlation/static/js/2.df818563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9572c3d4713c655b6541",
+    "url": "/covid-correlation/static/js/main.686c43c7.chunk.js"
+  },
+  {
+    "revision": "bc280f0b55ed3ce17cb6",
+    "url": "/covid-correlation/static/js/runtime-main.fc11ce5b.js"
+  }
+]);
